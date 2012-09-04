2e2ea666a08d8f5778c80d3d10873c3ab236e8a3
Philipp Blumers <kp2000@gmx.net>
2012-09-04 12:31:26 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobileorganizer::Application.config.secret_token = '6ac9cd43ebdccaecbeaee7add4e7a054e6c45b7b5b7f893e290ce33d3d1ad6663e10565e43d70a1b9fa8c115320619077ae779d803e7d60e0be661f0829030a3'
